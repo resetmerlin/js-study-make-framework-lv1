70d67bfb182b11e666554499cb3695647c1477f1
resetmerlin <resetmerlin@gmail.com>
2024-01-08 16:53:47 +0900

    feat: Add element props type

---
+++ react-todo/src/types/element-props.ts
@@ -0,0 +1,6 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps } from 'react'
+
+export type TButtonElementProps = DetailedHTMLProps<
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+>
