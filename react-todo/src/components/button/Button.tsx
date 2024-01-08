5f8248bf8d68d0e6415df738d33b1ce413fed468
resetmerlin <resetmerlin@gmail.com>
2024-01-08 16:53:15 +0900

    chore: Delete past files

--- react-todo/src/components/button/Button.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './Button.module.scss'
-
-export default function Button() {
-    return (
-        <button className={styles.button} type="button">
-            Add
-        </button>
-    )
-}
--- react-todo/src/components/container/Container.tsx
+++
@@ -1,6 +0,0 @@
-import { PropsWithChildren } from 'react'
-import styles from './Container.module.scss'
-
-export default function Container({ children }: PropsWithChildren) {
-    return <div className={styles.container}>{children}</div>
-}
