e5c28972fa1888a93201b424351cddcc48acc0dd
resetmerlin <resetmerlin@gmail.com>
2024-01-08 15:19:44 +0900

    feat: Add button component with scss file

---
+++ react-todo/src/components/button/Button.tsx
@@ -0,0 +1,9 @@
+import styles from './Button.module.scss'
+
+export default function Button() {
+    return (
+        <button className={styles.button} type="button">
+            Add
+        </button>
+    )
+}
