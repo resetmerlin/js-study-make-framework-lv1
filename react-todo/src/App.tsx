6289df0eb2aab85175bb82a80af4336b82098fa8
resetmerlin <resetmerlin@gmail.com>
2024-01-08 16:59:58 +0900

    feat: Add logic on todo app

--- react-todo/src/App.tsx
+++ react-todo/src/App.tsx
@@ -1,38 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useCallback, useState } from 'react'
+import { Container, ToDoList, TodoLists } from './components'
+
+type ITodoList = {
+    order: number
+    title: string
+    subtitle: string
+}
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [todoLists, setTodoLists] = useState<ITodoList[] | []>([])
+
+    const addTodoList = useCallback(() => {
+        const dummyData = {
+            order: todoLists?.length,
+            title: 'You can delete it',
+            subtitle: 'Edit via delete button',
+        }
+
+        setTodoLists((oldArray) => [...oldArray, dummyData])
+    }, [todoLists, setTodoLists])
+
+    const deleteTodoList = useCallback(
+        (order: number) => {
+            setTodoLists(
+                [...todoLists].filter((todoList) => order !== todoList.order)
+            )
+        },
+        [todoLists, setTodoLists]
+    )
 
     return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank" rel="noreferrer" />
-                <a href="https://react.dev" target="_blank" rel="noreferrer">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card ">
-                <button
-                    onClick={() => setCount((count1) => count1 + 1)}
-                    type="button"
-                >
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
-        </>
+        <Container>
+            <TodoLists addTodoList={addTodoList}>
+                {todoLists &&
+                    todoLists.map((todoList) => (
+                        <ToDoList
+                            key={todoList.order}
+                            title={todoList?.title}
+                            subtitle={todoList?.subtitle}
+                            deleteTodoList={deleteTodoList}
+                            order={todoList.order}
+                        />
+                    ))}
+            </TodoLists>
+        </Container>
     )
 }
 
