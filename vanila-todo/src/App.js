16be09c4dacc7c2fe649fab1a9479fa415fc3ea8
resetmerlin <resetmerlin@gmail.com>
2024-02-06 14:58:59 +0900

    feat: Add final code CRUD todo list

--- vanila-todo/src/App.js
+++ vanila-todo/src/App.js
@@ -1,21 +1,92 @@
-import { Button, Checkbox } from "./components";
+const $addTodoListButton = document.querySelector("#add__button");
+const $todoListContent = document.querySelector(".todolists__content");
 
-class App {
-  constructor() {
-    this.defineWebComponents();
-    this.render();
-  }
+$addTodoListButton.addEventListener("click", addTodoList);
 
-  defineWebComponents() {
-    customElements.define("button-component", Button);
-    customElements.define("checkbox-component", Checkbox);
-  }
+let todoLists = [];
 
-  render() {
-    const $app = document.getElementById("app");
-    $app.innerHTML =
-      "<button-component>TEST</button-component> <checkbox-component></checkbox-component>";
-  }
+function renderTodoList() {
+  $todoListContent.innerHTML = todoLists.map((todoList) =>
+    todoListView(todoList.title, todoList.subtitle, todoList.order)
+  );
+
+  todoLists.forEach((todoList) => {
+    const form = document.querySelector(`#form-${todoList.order}`);
+    form.addEventListener("submit", (e) => editTodoList(e, todoList.order));
+
+    const deleteButton = document.querySelector(`#delete-${todoList.order}`);
+    deleteButton.addEventListener("click", () =>
+      deleteTodoList(todoList.order)
+    );
+  });
 }
 
-new App();
+function addTodoList() {
+  const dummyData = {
+    order: todoLists?.length ? todoLists[todoLists.length - 1].order + 1 : 0,
+    title: "You can delete it",
+    subtitle: "Edit via delete button",
+  };
+
+  todoLists = [...todoLists, dummyData];
+
+  renderTodoList();
+}
+
+function deleteTodoList(order) {
+  todoLists = [...todoLists].filter((todoList) => order !== todoList.order);
+
+  renderTodoList();
+}
+
+function editTodoList(event, order) {
+  event.preventDefault();
+
+  const data = new FormData(event.target);
+  const dataObject = Object.fromEntries(data.entries());
+
+  const title = dataObject.title;
+  const subtitle = dataObject.subtitle;
+
+  if (!title && !subtitle) return;
+
+  todoLists = todoLists.map((todoList) => {
+    if (todoList.order === order) {
+      return { ...todoList, title, subtitle };
+    }
+    return todoList;
+  });
+
+  renderTodoList();
+}
+
+function todoListView(title, subtitle, order) {
+  // Note: Removed deleteTodoList argument since it's not directly used here
+  return `
+    <form id="form-${order}" class="todolist">
+        <div>
+          <label class="checkbox path">
+              <input type="checkbox" />
+              <svg viewBox="0 0 21 21">
+                  <path d="M5,10.75 L8.5,14.25 L19.4,2.3 C18.8333333,1.43333333 18.0333333,1 17,1 L4,1 C2.35,1 1,2.35 1,4 L1,17 C1,18.65 2.35,20 4,20 L17,20 C18.65,20 20,18.65 20,17 L20,7.99769186" />
+              </svg>
+          </label>
+        </div>
+        <div>
+              <input
+                  class="title"
+                  placeholder=${title}
+                  id="title"
+                  name="title" 
+              />
+              <input
+                  class="subtitle"
+                  placeholder=${subtitle}
+                  id="subtitle"
+                  name="subtitle" 
+              />
+      </div>    
+        <button type="submit" class='button'>Edit</button>
+        <button type="button" id="delete-${order}" class='button'>Delete</button>
+    </form>`;
+}
